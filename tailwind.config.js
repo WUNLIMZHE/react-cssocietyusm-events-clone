21fa27e872c51bf7a97420d999b8bfe9f2f29f03
WUN LIM ZHE <wunlimzhe2004@gmail.com>
2024-08-30 12:00:57 +0800

    -

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,10 @@ export default {
         'theme-100': 'rgb(255 228 105)',
         'base-200': '#dedede',
         'theme-700': 'rgb(22 101 106)',
-        'theme-800': '#103f45', // Replace with your desired color value
+        'theme-800': '#103f45', 
       },
       fontWeight: {
-        'medium': 490, // Custom font weight
+        'medium': 490,
       },
     },
   },
